77da3e14f44fb6f07eaf3c5b2910e1bb29293682
Gokul <gokulchandan24@gmail.com>
2025-10-20 15:48:14 +0300

    a

--- src/lib/apollo-client.ts
+++ src/lib/apollo-client.ts
@@ -2,7 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    uri: 'http://192.168.2.195:7000/graphql', // ✅ matches working app
+    uri: process.env.NEXT_PUBLIC_GRAPHQL_URL || 'http://192.168.2.195:7000/graphql',
 });
 
 const authLink = setContext((_, { headers }) => {
